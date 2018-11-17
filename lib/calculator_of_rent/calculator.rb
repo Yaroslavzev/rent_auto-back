bd0e74c8bc89065bdedcc7606265e8d4e0f0e8cc
Yaroslavzev <yaroslavzevalex@gmail.com>
2018-11-17 12:35:02 +0300

    code refactoring

--- lib/calculator_of_rent/calculator.rb
+++ lib/calculator_of_rent/calculator.rb
@@ -10,28 +10,18 @@ module CalculatorOfRent
       @end_time_day = end_time.to_date
     end
 
-    NUMBER_OF_WORK_DAYS_OF_WEEK = 5
-    NUMBER_OF_WEEKEND_DAYS = 2
-
-    def model_cost
-      @cost1 = 1000
-      @cost2 = 900
-      @cost3 = 800
-      @costworkday = 4500
-      @costweekend = 1800
-    end
-
     def logic_calculator
+      model_cost
       @rent_days = ((@end_time - @start_time)/1.day).to_i + 1
 
       if @rent_days <= 6
-        if (@start_time_day..@end_time_day).select {|d| (1..5).include?(d.wday)}.size === NUMBER_OF_WORK_DAYS_OF_WEEK
+        if work_days_of_week?
           if @rent_days === NUMBER_OF_WORK_DAYS_OF_WEEK + 1
             @cost = @costworkday + @cost1
           else
             @cost = @costworkday
           end
-        elsif (@start_time_day..@end_time_day).select {|d| 6.eql?(d.wday) or 0.eql?(d.wday) }.size === NUMBER_OF_WEEKEND_DAYS
+        elsif weekend_of_week?
           @cost = (@rent_days - NUMBER_OF_WEEKEND_DAYS) * @cost1 + @costweekend
         else
           @cost = @rent_days * @cost1
@@ -42,5 +32,29 @@ module CalculatorOfRent
         @cost = @rent_days * @cost3
       end
     end
+
+    private
+
+    NUMBER_OF_WORK_DAYS_OF_WEEK = 5
+    NUMBER_OF_WEEKEND_DAYS = 2
+    
+    def model_cost
+      @cost1 = 1000
+      @cost2 = 900
+      @cost3 = 800
+      @costworkday = 4500
+      @costweekend = 1800
+
+    end
+
+    def work_days_of_week?
+      (@start_time_day..@end_time_day).select {|d| (1..5).include?(d.wday)}.size === NUMBER_OF_WORK_DAYS_OF_WEEK
+    end
+
+
+    def weekend_of_week?
+      (@start_time_day..@end_time_day).select {|d| 6.eql?(d.wday) or 0.eql?(d.wday) }.size === NUMBER_OF_WEEKEND_DAYS
+    end
+
   end
 end
