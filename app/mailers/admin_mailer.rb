92968d9a36dfa29d110aa67153c6301643b7074c
Oleg Blashchuk <40044603+contras2k@users.noreply.github.com>
2018-11-25 16:47:41 +0300

    Маркетинговое наименование сайта в письмах вынесено в настройки сайта.

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,6 @@
 class AdminMailer < ApplicationMailer
   def request_email
     @p = params[:parameters]
-    mail(to: ENV['ADMIN_EMAIL'], subject: I18n.t('mailer.subjects.request.new'))
+    mail(to: ENV['ADMIN_EMAIL'], subject: I18n.t('mailer.subjects.request.new') % {site: Rails.configuration.site_name})
   end
 end
