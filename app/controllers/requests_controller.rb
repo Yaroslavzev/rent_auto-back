800488353801b1ee056df44cd177ab1799a173b0
Oleg Blashchuk <40044603+contras2k@users.noreply.github.com>
2018-11-30 10:43:11 +0300

    В теме письма администратору добавлено имя сайта, с которого идёт заявка (на случай множества возможных источников заявок)

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -9,7 +9,7 @@ class RequestsController < ApplicationController
     rp = Hashie::Mash.new(params)
     # TODO: это временно до 01.11.18 - M.L. заменит model_name на model (и урежет список полей)
     rp.model = Hashie::Mash.new(rp.model_name? ? rp.model_name : rp.model)
-    AdminMailer.with(parameters: rp).request_email.deliver_now
+    AdminMailer.with(parameters: rp, source: request.host).request_email.deliver_now
     # в настоящий момент отправляем письмо клиенту, если он просто заполнил поле e-mail
     # TODO: проверять адрес на валидность
     # TODO: анти-спам защита (например, отправлять только для зарегистрированных пользователей или капча)
--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,7 @@
 class AdminMailer < ApplicationMailer
   def request_email
     @p = params[:parameters]
-    mail(to: ENV['ADMIN_EMAIL'], subject: I18n.t('mailer.subjects.request.new') % {site: Rails.configuration.site_name})
+    mail(to: ENV['ADMIN_EMAIL'], subject: format(I18n.t('mailer.subjects.request.new'),
+                                                 site: Rails.configuration.site_name, host: params[:source]))
   end
 end
