4e53ebf1bdf1ddfafedcd1e11c655abb9d202e6d
Oleg Blashchuk <40044603+contras2k@users.noreply.github.com>
2018-11-30 20:14:06 +0300

    Указание списка заказанных дополнение (Accessories and Services) в письме администратору. Пока заложил оба возможнных варианта поля - массив строк (старый вариант) и массив хэшей (будет после 01.11.18).

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -10,9 +10,13 @@ class RequestsController < ApplicationController
     # TODO: это временно до 01.11.18 - M.L. заменит model_name на model (и урежет список полей)
     rp.model = Hashie::Mash.new(rp.model_name? ? rp.model_name : rp.model)
 
+    rp.additions = [] unless rp.additions?
+    # TODO: после 01.11.18 тут вообще не будет от фронта строк, всегда хэш. Template тоже упростить.
+    rp.additions.map! { |aas| (aas.instance_of? String) ? aas : Hashie::Mash.new(aas) }
+
     %i[begin_time end_time birthdate doc_issued_date lic_date lic_valid_to].each do |t|
       rp[t] = Time.iso8601(rp[t]) if rp[t]
-    rescue ArgumentError # если параметр не является валидной датой
+    rescue ArgumentError # если параметр не является валидной датой, то вообще обнуляем его
       rp[t] = nil
     end
 
