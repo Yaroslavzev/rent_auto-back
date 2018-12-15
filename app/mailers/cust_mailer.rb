d4a599e9ae0831bd8427f1bc67477c19690ec6be
Oleg Blashchuk <40044603+contras2k@users.noreply.github.com>
2018-12-15 16:34:29 +0300

    Основное - в контроллере заявок добавлена возможность экспорта заявки во внешнюю БД, если она настроена. Также разгружен метод create - настройки и валидации параметров перенесены в отдельные private методы. Ну и бонусом от экспорта заявок - появились номера заявок, которые отображаем в HTTP response и в письмах.

--- app/mailers/cust_mailer.rb
+++ app/mailers/cust_mailer.rb
@@ -2,6 +2,8 @@
 class CustMailer < ApplicationMailer
   def request_email
     @p = params[:parameters]
+    return unless @p.email&.match?(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i)
+
     mail(to: @p.email, subject: format(I18n.t('mailer.subjects.request.got-it'), site: Rails.configuration.site_name))
   end
 end
