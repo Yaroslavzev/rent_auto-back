cae3ab85b52066e168d1689bef9d91db3bd05eba
Oleg Blashchuk <40044603+contras2k@users.noreply.github.com>
2018-12-15 16:28:25 +0300

    Подключение внешней БД стало опциональным - если в .env не задано имя базы в MySQL (DATABASE_PS_NAME), то не создаём конфигурацию подключения (и далее не будем пытаться экспортировать заявки).

--- config/initializers/database_psoft.rb
+++ config/initializers/database_psoft.rb
@@ -1,2 +1,3 @@
 # Загрузить конфигурацию БД обмена PSoft
-PSOFT_DB = YAML.load(ERB.new(File.read(Rails.root.join("config","database_psoft.yml"))).result)[Rails.env]
+db = YAML.load(ERB.new(File.read(Rails.root.join('config', 'database_psoft.yml'))).result)[Rails.env]
+PSOFT_DB = db['database'] ? db : nil
