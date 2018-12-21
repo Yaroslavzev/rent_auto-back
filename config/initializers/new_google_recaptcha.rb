70d61b0ce347270e3a2c19822cd4ea02ada2691a
Yaroslavzev <yaroslavzevalex@gmail.com>
2018-12-21 22:49:54 +0300

    Добавил поддержку капчи

---
+++ config/initializers/new_google_recaptcha.rb
@@ -0,0 +1,6 @@
+if Object.const_defined?('NewGoogleRecaptcha')
+  NewGoogleRecaptcha.setup do |config|
+    config.site_key   = "6LcOPX4UAAAAAMKNr7yUMt2I8vX6YY9FGggIhwHD"
+    config.secret_key = "6LcOPX4UAAAAABpby-VvmyIFwh_YyKr6PJP0tKdN"
+  end
+end
