20ff609d8026301efdf68420175f14db5a796508
Yaroslavzev <yaroslavzevalex@gmail.com>
2018-12-21 22:09:48 +0300

    незначительные изменения

--- app/controllers/rent_values_controller.rb
+++ app/controllers/rent_values_controller.rb
@@ -2,7 +2,7 @@ class RentValuesController < ApplicationController
   def index
     @calculation = ::CalculatorOfRent::Calculator.new(post_params['start_time'], post_params['end_time'], post_params['model'])
 
-    @cost = @calculation.logic_calculator
+    @cost = @calculation.logic_calculator.to_i
     render json: @cost
   end
 
