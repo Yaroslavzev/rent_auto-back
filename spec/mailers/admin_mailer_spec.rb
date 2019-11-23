f6111e1a32599f4780a3d0e89c004555397ef3af
Yaroslavzev <yaroslavzevalex@gmail.com>
2019-11-23 22:38:50 +0300

    minor fix

--- spec/mailers/admin_mailer_spec.rb
+++ spec/mailers/admin_mailer_spec.rb
@@ -1,5 +1,8 @@
 require "rails_helper"
 
 RSpec.describe AdminMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it "works" do
+    puts "it works"
+  end
 end
